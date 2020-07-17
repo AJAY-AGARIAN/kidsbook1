01d73c521030e953a0acdad656a7b934564a84e0
abin shibu thomas <abinshibuthomas67@gmail.com>
2020-07-17 22:36:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/spellingbook/alphabets2screen.java
@@ -0,0 +1,69 @@
+
+package com.example.spellingbook;
+
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.GridView;
+        import android.widget.Toast;
+
+public class alphabets2screen extends AppCompatActivity {
+    GridView grill;
+    String[] numberword={"","",""," ","","","","","","","","","","","","","","","","",
+            "","","","","",""};
+    int[] numberimage={
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+            R.drawable.f,
+            R.drawable.g,
+            R.drawable.h,
+            R.drawable.i,
+            R.drawable.j,
+            R.drawable.k,
+            R.drawable.l,
+            R.drawable.m,
+            R.drawable.n,
+            R.drawable.o,
+            R.drawable.p,
+            R.drawable.q,
+            R.drawable.r,
+            R.drawable.s,
+            R.drawable.t,
+            R.drawable.u,
+            R.drawable.v,
+            R.drawable.w,
+            R.drawable.x,
+            R.drawable.y,
+            R.drawable.z};
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabets2screen);
+
+        grill=findViewById(R.id.gridview);
+        mainAdapter adapter= new mainAdapter(alphabets2screen.this,numberword,numberimage);
+        grill.setAdapter(adapter);
+
+
+
+        grill.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"You Clicked"+numberword[+position],
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
